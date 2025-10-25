7be19c9183039f1ae41b371d5eaaf9d2ad98fc98
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2025-10-25 19:13:40 +0530

    change product.php

--- InventorySystem_PHP/product.php
+++ InventorySystem_PHP/product.php
@@ -44,7 +44,7 @@
                   <img class="img-avatar img-circle" src="uploads/products/<?php echo $product['image']; ?>" alt="">
                 <?php endif; ?>
                 </td> -->
-                <td> <?php echo remove_junk($product['name']); ?></td>
+                <td> <?php echo remove_junk($product['name']); ?> </td>
                 <td class="text-center"> <?php echo remove_junk($product['category_name']); ?></td>
                 <td class="text-center"> <?php echo remove_junk($product['quantity']); ?></td>
                 <td class="text-center"> <?php echo remove_junk($product['buy_price']); ?></td>
