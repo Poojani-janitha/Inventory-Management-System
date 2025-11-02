774ba72dd4c2b91199f8bcfd07ee7ffc5c277379
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-11-02 11:04:19 +0530

    add permission to staffmember to return

--- InventorySystem_PHP/auto_expiry_checker.php
+++ InventorySystem_PHP/auto_expiry_checker.php
@@ -2,7 +2,7 @@
   $page_title = 'Auto Expiry Checker';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
-  page_require_level(1);
+  page_require_level(2);
   //extra add prabashi 
     $msg = $session->msg();
   
