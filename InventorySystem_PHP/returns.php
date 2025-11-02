508d0b4e3aaa24692d5b98d15e5c62f3d6e5204d
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-11-02 11:15:22 +0530

    change permission:

--- InventorySystem_PHP/returns.php
+++ InventorySystem_PHP/returns.php
@@ -3,6 +3,7 @@
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
    page_require_level(2);
+    page_require_level(1);
   //extra add prabashi 
     $msg = $session->msg();
 ?>
